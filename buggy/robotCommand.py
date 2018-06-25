f6a830483e767bec7753fd811ab5eaf2804161cd
melt <thomas.meltzer1@gmail.com>
2018-06-25 16:57:17 +0000

    added amazon voice

---
+++ buggy/robotCommand.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+import json
+import requests
+import time
+import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, "/robot_commands")
+
+
+@app.route('/')
+def homepage():
+    return "hi there, how ya doin?"
+
+@ask.launch
+def start_skill():
+    welcome_message = 'Hey homies, I am the best robot'
+    return question(welcome_message)
+
+@ask.intent("ForwardIntent")
+def forward_intent(ForwardNumber):
+    print(ForwardNumber)
+    go_msg= 'The sky is the limit, moving forward'
+    return statement(go_msg)
+
+
+@ask.intent("BackwardIntent",convert={"BackwardNumber":int})
+def backward_intent(BackwardNumber):
+    go_msg= 'I am not a coward but moving forward {}'.format(BackwardNumber)
+    return statement(go_msg)
+
+
+
+
+@ask.intent("NoIntent")
+def no_intent():
+    bye_text = 'I am not sure why you asked me to run then, but okay... bye'
+    return statement(bye_text)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
