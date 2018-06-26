830a0bdde9fd3d54878da9d6b4d0104e58802446
Tom Melt <thomas.meltzer1@gmail.com>
2018-06-26 16:18:13 +0100

    testing

--- buggy/camera.py
+++ buggy/camera.py
@@ -4,11 +4,10 @@ from time import sleep
 
 def picture():
     name = 'picture.jpg'
-    with open(name, 'w') as outfile:
-        camera = PiCamera()
-        camera.start_preview()
-        sleep(1)
-        camera.capture(outfile)
-        camera.stop_preview()
-        camera.close()
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(1)
+    camera.capture(name)
+    camera.stop_preview()
+    camera.close()
     return name
